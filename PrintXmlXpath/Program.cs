f83ad4b610a9b3460d81bf576303423e8d96dfca
Ian Carpenter <ianhcarpenter@gmail.com>
2017-05-15 21:25:04 +0100

    Initial Commit

---
+++ PrintXmlXpath/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml;
+
+namespace PrintXmlXpath
+{
+    /// <summary>
+    /// Utility to print out the XPath location of interest
+    /// Default use case is to find elements that have a text node that exceeds
+    /// 20 characters.
+    /// Requires one command line parameter; the path to look for the xml document(s)
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            const string theXpath = "//*/text()[string-length() > 20]/..";
+
+            Utils utils = new Utils();
+
+            string[] files = utils.getFiles(args[0]);
+
+            foreach (string file in files)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(file);
+                XmlNode node = doc.SelectSingleNode(theXpath);
+
+                // found something so print out the filename, xpath 
+                // and text 
+                if(node != null)
+                {
+                    Console.WriteLine(file);
+                    Console.WriteLine(utils.FindXPath(node));
+                    Console.WriteLine(node.InnerText);
+                    Console.WriteLine();
+                }                
+
+            }
+            Console.Read();
+        }               
+    }
+}
---
+++ PrintXmlXpath/Utils.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace PrintXmlXpath
+{
+    public class Utils
+    {
+        /// <summary>
+        /// for the supplied path return files that have a .xml extension
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public string[] getFiles(string path)
+        {
+            string[] files = Directory.GetFiles(path, "*.xml");
+
+            return files;
+        }
+
+        /// <summary>
+        /// Answer provided by Jon Skeet 
+        /// http://stackoverflow.com/a/241291/55640
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public string FindXPath(XmlNode node)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            while (node != null)
+            {
+                switch (node.NodeType)
+                {
+                    case XmlNodeType.Attribute:
+                        builder.Insert(0, "/@" + node.Name);
+                        node = ((XmlAttribute)node).OwnerElement;
+                        break;
+                    case XmlNodeType.Element:
+                        int index = FindElementIndex((XmlElement)node);
+                        builder.Insert(0, "/" + node.Name + "[" + index + "]");
+                        node = node.ParentNode;
+                        break;
+                    case XmlNodeType.Document:
+                        return builder.ToString();
+                    default:
+                        throw new ArgumentException("Only elements and attributes are supported");
+                }
+            }
+            throw new ArgumentException("Node was not in a document");
+        }
+
+        /// <summary>
+        /// http://stackoverflow.com/a/241291/55640
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        int FindElementIndex(XmlElement element)
+        {
+            XmlNode parentNode = element.ParentNode;
+            if (parentNode is XmlDocument)
+            {
+                return 1;
+            }
+            XmlElement parent = (XmlElement)parentNode;
+            int index = 1;
+            foreach (XmlNode candidate in parent.ChildNodes)
+            {
+                if (candidate is XmlElement && candidate.Name == element.Name)
+                {
+                    if (candidate == element)
+                    {
+                        return index;
+                    }
+                    index++;
+                }
+            }
+            throw new ArgumentException("Couldn't find element within parent");
+        }
+    }
+}
